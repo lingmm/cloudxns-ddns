c4dc4a1f8175126b8d2b6fd72b08c6064551e8fa
泠 <i@shyling.com>
2017-09-05 15:18:38 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+from __future__ import print_function
+from cloudxns import api
+import requests
+import re
+import json
+
+DOMAIN_ID = ''
+HOST = ''
+API_KEY = ''
+SECRET_KEY = ''
+
+def my_ip():
+    raw = requests.get('http://ip.cn', headers={'User-Agent': 'curl/7.54.0'}).text
+    return re.findall(r'[\d\.]+', raw)[0]
+
+def main():
+    xns = api.Api(API_KEY, SECRET_KEY)
+    record_list = json.loads(xns.record_list(DOMAIN_ID))
+    for record in record_list['data']:
+        if record['host'] != HOST:
+            continue
+        xns.record_update(record['record_id'], DOMAIN_ID, HOST, my_ip(), ttl= 60)
+        print('DNS updated')
+
+if __name__ == '__main__':
+    main()
